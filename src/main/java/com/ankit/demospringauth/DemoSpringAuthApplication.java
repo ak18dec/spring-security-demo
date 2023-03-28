045a63a8f53495f08644b1cc83765c6eed4d8ff5
Ankit <ankit.dayal1992@gmail.com>
2023-03-28 21:35:43 +0530

    Project Setup

---
+++ src/main/java/com/ankit/demospringauth/DemoSpringAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ankit.demospringauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ankit/demospringauth/DemoSpringAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.demospringauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
