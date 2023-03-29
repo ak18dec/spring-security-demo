6505873c61562d4281a0bd8fcf01304774751740
Ankit <ankit.dayal1992@gmail.com>
2023-03-29 18:05:13 +0530

    Updating Autowired Dependencies to Interface First

--- src/main/java/com/ankit/demospringauth/WebSecurity.java
+++ src/main/java/com/ankit/demospringauth/WebSecurity.java
@@ -1,6 +1,7 @@
 package com.ankit.demospringauth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,18 +9,20 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
 
+    @Qualifier("userDetailsServiceImpl")
     @Autowired
-    UserDetailsServiceImpl userDetailsService;
+    UserDetailsService userDetailsService;
 
     @Autowired
-    BCryptPasswordEncoder encoder;
+    PasswordEncoder encoder;
 
 
 
